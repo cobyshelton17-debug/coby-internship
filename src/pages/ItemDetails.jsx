916b362986d698bb11def335c8d591030aff3826
Coby Shelton <cobyshelton@Cobys-MacBook-Air.local>
2026-08-31 11:28:16 -0500

    nftDetails

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,16 +1,89 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 import EthImage from "../images/ethereum.svg";
-import { Link, useLocation } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import Skeleton from "../components/UI/Skeleton";
 
 const ItemDetails = () => {
-  const location = useLocation();
-  const collection = location.state?.collection;
+  const { nftId } = useParams();
+
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    setItem(null);
+    axios
+      .get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+      )
+      .then((res) => {
+        if (!res.data) {
+          setError(true);
+          return;
+        }
+        setItem(res.data);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [nftId]);
+
+  if (loading) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6 text-center">
+                  <Skeleton width="100%" height="420px" borderRadius="12px" />
+                </div>
+                <div className="col-md-6">
+                  <div className="item_info">
+                    <Skeleton width="60%" height="36px" borderRadius="6px" />
+                    <div className="spacer-10"></div>
+                    <Skeleton width="90%" height="16px" borderRadius="4px" />
+                    <Skeleton width="85%" height="16px" borderRadius="4px" />
+                    <Skeleton width="88%" height="16px" borderRadius="4px" />
+                    <div className="spacer-20"></div>
+                    <Skeleton width="120px" height="20px" borderRadius="4px" />
+                    <div className="spacer-10"></div>
+                    <Skeleton width="140px" height="20px" borderRadius="4px" />
+                    <div className="spacer-20"></div>
+                    <Skeleton width="100px" height="32px" borderRadius="6px" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+
+  if (error || !item) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container text-center">
+              <h3>Sorry, this item could not be found.</h3>
+              <p>The NFT you are looking for may not exist or is unavailable.</p>
+              <Link className="btn-main" to="/explore">
+                Back to Explore
+              </Link>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
@@ -21,48 +94,44 @@ const ItemDetails = () => {
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={collection ? collection.nftImage : nftImage}
+                  src={item.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
+                  alt={item.title}
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>
-                    {collection ? collection.title : "Rainbow Style #194"}
-                  </h2>
+                  <h2>{item.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {item.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {item.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                  <p>{item.description}</p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
+                          <Link to={`/author/${item.ownerId}`}>
                             <img
                               className="lazy"
-                              src={collection ? collection.authorImage : AuthorImage}
+                              src={item.ownerImage}
                               alt=""
                             />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${item.ownerId}`}>
+                            {item.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -73,17 +142,19 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
+                          <Link to={`/author/${item.creatorId}`}>
                             <img
                               className="lazy"
-                              src={collection ? collection.authorImage : AuthorImage}
+                              src={item.creatorImage}
                               alt=""
                             />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${item.creatorId}`}>
+                            {item.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -91,7 +162,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{item.price.toFixed(2)}</span>
                     </div>
                   </div>
                 </div>
